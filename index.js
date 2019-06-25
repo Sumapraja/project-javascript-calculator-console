108488a208171fb17138283c7d2d9e7d3616351c
Sumapraja <irfansumapraja@gmail.com>
2019-06-25 19:03:41 +0700

    added prompt

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const digitA = 100
-const digitB = 200.5
-
-const resultAddition = digitA + digitB
-const resultSubstraction = digitA - digitB
-const resultMultiplication = digitA * digitB
-const resultDivison = digitA / digitB
+let digitA = parseInt(prompt('input your first number'))
+let operators = prompt('what are you trying to do? :' + '\n 1. Addition' + '\n 2. Substraction' + '\n 3. Multiplication' + '\n 4. Division')
+let digitB = parseInt(prompt('input your second number'))
 
 console.log(digitA)
+console.log(operators);
 console.log(digitB)
 
-console.log(resultAddition)
-console.log(resultSubstraction)
-console.log(resultMultiplication)
-console.log(resultDivison)
+if (operators == '1') {
+    const resultAddition = digitA + digitB
+    console.log(resultAddition)
+    alert(digitA + digitB)
+    
+}  else if (operators == '2') {
+    const resultSubstraction = digitA - digitB
+    console.log(resultSubstraction)
+    alert(digitA - digitB)
+
+}   else if (operators == '3') {
+    const resultMultiplication = digitA * digitB
+    console.log(resultMultiplication)
+    alert(digitA * digitB)
+
+}   else if (operators == '4') {
+    const resultDivison = digitA / digitB
+    console.log(resultDivison)
+    alert(digitA / digitB)
+}   
+    else {
+    alert('sorry cannot operate, make sure you choose with the number')
+}
+
+
+
+
+
+
+
+
+
+
+
+
